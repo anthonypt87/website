ca551967f3f73d3248a3b43bb6b2d73b45b6d329
anthony <anthonypt87@gmail.com>
2013-02-03 23:32:44 -0800

    Adding automatic admin for blog

--- website/blog/tests.py
+++ website/blog/tests.py
@@ -9,18 +9,20 @@ class BlogModelTest(TestCase):
 			title='category_title',
 			slug='category_slug'
 		)
+		self.created_category.save()
 		self.created_blog = models.Blog(
 			title='Title',
 			slug='Slug',
 			body='Body',
-			category=self.created_category.id
+			category=self.created_category
 		)
+		self.created_blog.save()
 
 	def tearDown(self):
 		self.created_blog.delete()
 
 	def test_model_works(self):
-		loaded_blog = self.created_blog.get(id=self.created_blog.id)
+		loaded_blog = models.Blog.objects.get(id=self.created_blog.id)
 		self.assertEqual(
 			loaded_blog,
 			self.created_blog
