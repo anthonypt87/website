066221465abdd78078aff6ce37ae1bb46cbd35f6
anthony <anthonypt87@gmail.com>
2013-02-07 01:15:19 -0800

    Added simple template

--- website/blog/views.py
+++ website/blog/views.py
@@ -1,4 +1,13 @@
+from blog.models import Blog, Category
+from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
+	context = {
+		'categories': Category.objects.all(),
+		'posts': Blog.objects.all()
+	}
+	return render(request, 'blog/index.html', context)
+
+def view_post(request, blug_id):
 	return HttpResponse('hi')
