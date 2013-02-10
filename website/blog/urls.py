bef83422e0d50950e4af18db072df2ff8ad259dc
anthony <anthonypt87@gmail.com>
2013-02-09 22:54:31 -0800

    Add urls

---
+++ website/blog/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import patterns, url
+from blog import views
+
+urlpatterns = patterns(
+	url(r'^$', views.index, name='index'),
+	url(r'^(?P<blog_id>\d+)/$', views.view_post, name='view_post'),
+)
